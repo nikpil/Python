1320e44fffb6676614f54cde66fe1cc394346f44
Nikpil <77510423+nikpil@users.noreply.github.com>
2021-02-01 17:05:30 +0300

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,10 @@
+my_Name = 'Николай'
+my_Surname = 'Пелипенко'
+print("Меня зовут", my_Name, my_Surname)
+name = input('Напишите свое имя ')
+surname = input('Напишите свою фамилию ')
+day = int(input("Укажите день своего рождения "))
+month = input('Укажите месяц своего рождения ')
+year = int(input("Укажите год своего рождения "))
+
+print(f"{name} {surname}, здравствуйте!\nВы родились {day}.{month}.{year}  ")
